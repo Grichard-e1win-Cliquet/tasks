c8e0fc2c390bffb4a7986a3572097af9b7057c8e
e1win <e1win@mail.ru>
2023-04-10 23:20:08 +0300

    add Task 5. Array from -x to x

---
+++ tasks/task 5/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Enter a number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x > 0)
+{
+    for (int i=-x; i < x + 1; i++)
+    {
+    Console.Write($"{i},");
+    }
+Console.WriteLine();
+}
+else
+{
+
+    for (int i=x; i < -x + 1; i++)
+    {
+    Console.Write($"{i},");
+    }
+Console.WriteLine();
+}
