851ddd26289d82f241ce0c2bba08a84fd27b17fc
e1win <e1win@mail.ru>
2023-04-10 23:56:34 +0300

    add Task 7. %n

---
+++ tasks/task 7/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Enter a three-digits number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if(-99 > x || x > 99)
+{
+    Console.WriteLine($"Last digit is {x%10}");
+}
+else
+{
+    Console.WriteLine("Three-digits number, punk");
+}
