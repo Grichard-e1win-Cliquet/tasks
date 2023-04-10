12dd68f85d42e9ca83255907e203861d869f1abf
e1win <e1win@mail.ru>
2023-04-10 18:40:53 +0300

    add Task3. Day of the week

---
+++ tasks/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Enter a number of day of the week: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x == 1) Console.WriteLine("Today is Monday");
+if (x == 2) Console.WriteLine("Today is Tuesday");
+if (x == 3) Console.WriteLine("Today is Wednesday");
+if (x == 4) Console.WriteLine("Today is Thursday");
+if (x == 5) Console.WriteLine("Today is Friday");
+if (x == 6) Console.WriteLine("Today is Saturday");
+if (x == 7) Console.WriteLine("Today is Sunday");
