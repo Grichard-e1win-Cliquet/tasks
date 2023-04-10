8b8972b6653e832fac6462d19d52e54d554c8661
e1win <e1win@mail.ru>
2023-04-10 23:43:02 +0300

    added info in task 5 about Math.Abs

--- tasks/task 5/Program.cs
+++ tasks/task 5/Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.Clear();
 Console.WriteLine("Enter a number: ");
 int x = Convert.ToInt32(Console.ReadLine());
+// int x = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 if (x > 0)
 {
     for (int i=-x; i < x + 1; i++)
