27dd43ff3d6f49bf367e1523c0724572f34d8914
e1win <e1win@mail.ru>
2023-04-10 18:07:10 +0300

    add Task01 about square true/false

---
+++ tasks/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("enter two numbers ");
+Console.Write("enter the first: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("enter the second: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a == b*b)
+{
+    Console.WriteLine($"Yes! The {a} is a squre of {b}");
+}
+else
+{
+    Console.WriteLine($"No! The {a} is not a squre of {b}");
+}
