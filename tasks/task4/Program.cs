bd616b8cc34f6828cfda8735b10dd3403799115c
e1win <e1win@mail.ru>
2023-04-12 14:42:56 +0300

    add task 4. max from 3 numbers

---
+++ tasks/task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter three numbers. ");
+
+Console.Write("Enter a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine($"max equals {max}");
