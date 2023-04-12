6da617ea2a7c67623b3616860dd88e5d9acae2d6
e1win <e1win@mail.ru>
2023-04-12 14:50:36 +0300

    add task 6. Even numbers

---
+++ tasks/task6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Enter a number: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a%2==0)
+{
+    Console.WriteLine($"Yes! {a} is an even number.");
+}
+else
+{
+    Console.WriteLine($"No! {a} is not an even number.");
+}
