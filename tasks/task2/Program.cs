077889730316f3f488cdf1ea5fdc61bbf57ba3af
e1win <e1win@mail.ru>
2023-04-12 14:31:27 +0300

    add Task 2. max from two numbers

---
+++ tasks/task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Enter two numbers. ");
+Console.Write("Enter a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) Console.WriteLine($"max is {a}");
+if (a == b) Console.WriteLine($"both numbers is max because it's equal {a}");
+if (a < b) Console.WriteLine($"max is {b}");
