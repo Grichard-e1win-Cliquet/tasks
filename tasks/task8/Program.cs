9d2de7e497b863d8f458f15d6a6071be29d34d2b
e1win <e1win@mail.ru>
2023-04-12 15:35:11 +0300

    add task 8. from 1 to N

---
+++ tasks/task8/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+Console.Write("Enter a number: ");
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x > 1)
+{
+    for (int i=2; i < x + 1; i+=2)
+    {
+    // Console.Write($"{i},");
+    Console.Write(i+" ");
+    }
+Console.WriteLine();
+}
+else if (x < -1)
+{
+    for (int i=-2; i > x -1; i+=-2)
+    {
+    // Console.Write($"{i},");
+    Console.Write(i+" ");
+    }
+}
+else
+{
+    Console.Write($"There is no even numbers from 1 to {x}");
+}
+Console.WriteLine();
