68a6079d4ce86977de4c6b4264bcc850356bc503
e1win <e1win@mail.ru>
2023-04-19 18:33:42 +0300

    add Task 9. There is about max digit from random number from 10 to 99.

---
+++ tasks/task9/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.Clear();
+int x = new Random().Next(10, 100);
+
+Console.WriteLine($"Random number is {x}");
+
+void FillArray(int[] number)
+{
+    int length = number.Length;
+    int index = 0;
+    while (index < length)
+    {
+        if (index == 0)
+        {
+            number[index] = x/10;
+            index++;
+        }
+        else
+        {
+            number[index] = x%10;
+            index++;
+        }
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]}");
+        position++;
+    }
+}
+
+int[] number = new int[2];
+FillArray(number);
+Console.Write("We have two digits in ");
+PrintArray(number);
+
+int max = number[0];
+if (max > number[1])
+{
+    Console.Write($". Max of these digits is {max}.");
+}
+else if (max < number[1]) 
+{
+    max = number[1]; Console.Write($". Max of these digits is {max}.");
+}
+else
+{
+    Console.Write(". These are both max.");
+}
+Console.WriteLine();
