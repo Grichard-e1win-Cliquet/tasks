3f676ba862b012f94496824f8f09cf7828a825ac
e1win <e1win@mail.ru>
2023-04-25 18:20:50 +0300

    added Task 12 with a good solution

---
+++ tasks/task12/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter two numbers. ");
+
+Console.Write("Enter a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a%b == 0)
+    {
+        Console.Write($"The {a} is a multiple of {b}.");
+    }
+else
+    {
+        Console.Write($"The {a} is not a multiple of {b}. The remainder equals {a%b}.");
+    }
+Console.WriteLine();
