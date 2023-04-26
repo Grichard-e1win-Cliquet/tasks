e0e4704846a31c0f051043d93034e1d8bcc94868
e1win <e1win@mail.ru>
2023-04-26 17:00:06 +0300

    added Task 16 about || (works when we use if/while/etc and means "if one of the states is yes")

---
+++ tasks/task16/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter two numbers. ");
+
+Console.Write("Enter a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a*a == b || b*b == a)
+    {
+        Console.Write($"The one of the numbers is a square root of the other.");
+    }
+else
+    {
+        Console.Write($"No! Neither number is the square root of the other.");
+    }
+
+// if (a*a == b)
+//     {
+//         Console.Write($"The {b} is a square root of {a}.");
+//     }
+// else if (b*b == a)
+//     {
+//         Console.Write($"The {a} is a square root of {b}.");
+//     }
+// else
+//     {
+//         Console.Write($"No! Neither number is the square root of the other.");
+//     }
+Console.WriteLine();
