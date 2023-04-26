50a4d6636a715ef4831d5fb52c2ea920b76a8104
e1win <e1win@mail.ru>
2023-04-26 17:40:54 +0300

    added decision to task 10 with string

--- tasks/task10/Program.cs
+++ tasks/task10/Program.cs
@@ -1,11 +1,22 @@
 ﻿Console.Clear();
 Console.WriteLine("Enter a three digits number:");
 
-int x = Convert.ToInt32(Console.ReadLine());
+// int x = Convert.ToInt32(Console.ReadLine());
 
+// if (Math.Abs(x) > 99 & Math.Abs(x) < 1000 )
+// {
+//     Console.WriteLine($"The second digit from {x} is {Math.Abs(x%100/10)}.");
+// }
+// else
+// {
+//     Console.WriteLine("I SAID THREE DIGITS NUMBER, PUNK!");
+// }
+
+int x = Convert.ToInt32(Console.ReadLine());
+string y = Convert.ToString(x);
 if (Math.Abs(x) > 99 & Math.Abs(x) < 1000 )
 {
-    Console.WriteLine($"The second digit from {x} is {Math.Abs(x%100/10)}.");
+    Console.WriteLine($"The second digit from {x} is {y[1]}.");
 }
 else
 {
