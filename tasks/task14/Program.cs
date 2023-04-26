c501dcc0dc93b0ed9f17e85cdfaeed471ee2c6a3
e1win <e1win@mail.ru>
2023-04-26 16:47:19 +0300

    added Task 14 about & and && when "if/while/etc"

---
+++ tasks/task14/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter a number: ");
+
+Console.Write("");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 7 == 0 && a % 23 == 0)
+    {
+        Console.Write($"The {a} is a multiple of 7 and 23.");
+    }
+else
+    {
+        Console.Write($"The {a} is not a multiple of 7 and 23.");
+    }
+Console.WriteLine();
