36e786e427a592e07fb7fbceddf6267926f12c6f
e1win <e1win@mail.ru>
2023-04-26 17:34:48 +0300

    added task 10 with Math.Abs and &

---
+++ tasks/task10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Enter a three digits number:");
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (Math.Abs(x) > 99 & Math.Abs(x) < 1000 )
+{
+    Console.WriteLine($"The second digit from {x} is {Math.Abs(x%100/10)}.");
+}
+else
+{
+    Console.WriteLine("I SAID THREE DIGITS NUMBER, PUNK!");
+}
