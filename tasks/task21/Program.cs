fe90e13ce5c4669eb9af6ea9401f281adeb44365
e1win <e1win@mail.ru>
2023-05-25 19:05:20 +0300

    added task 21. Distance from two points

---
+++ tasks/task21/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter coordinates XY for two points(A and B). ");
+
+Console.Write("Enter Xa = ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Ya = ");
+int ya = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter Xb = ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Yb = ");
+int yb = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt((Math.Pow((xa-xb), 2) + Math.Pow((ya-yb), 2)));
+
+Console.WriteLine($"The distance from point A to point B is {Math.Round(d, 2)}");
---
+++ tasks/task22/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter number of quarter. ");
