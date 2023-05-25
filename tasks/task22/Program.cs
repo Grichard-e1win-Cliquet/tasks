f685abb816dcb9ac43b8728484bd30a37ab883a1
e1win <e1win@mail.ru>
2023-05-25 19:28:44 +0300

    added task22. table of Pows

--- tasks/task22/Program.cs
+++ tasks/task22/Program.cs
@@ -1,3 +1,12 @@
 ﻿Console.Clear();
 
-Console.WriteLine("Enter number of quarter. ");
+Console.WriteLine("Enter a number. ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+for (int i = 1; i <= N; i++)
+{
+        Console.Write($"{Math.Pow(i, 2)} ");
+}
+Console.WriteLine();
