e1af22ee81a66ab81d444a7383e6e41196247fb2
e1win <e1win@mail.ru>
2023-05-26 18:34:54 +0300

    added task19. Polyndromes

---
+++ tasks/task19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter five digits number: ");
+
+int a = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+string x = Convert.ToString(a);
+if (Math.Abs(a)>9999 & Math.Abs(a)<99999)
+{
+    if (x[0]==x[4] & x[1]==x[3]) Console.WriteLine($"Number {x} is a polyndrom.");
+    else Console.WriteLine($"Number {x} is not a polyndrom.");
+}
+else Console.WriteLine($"Number {x} isn't five digits number.");
