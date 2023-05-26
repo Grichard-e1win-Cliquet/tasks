7364f416ca4785094ad1d1397cd5cea9448bb428
e1win <e1win@mail.ru>
2023-05-26 18:15:31 +0300

    added task21 3d decision

--- tasks/task21/Program.cs
+++ tasks/task21/Program.cs
@@ -1,17 +1,23 @@
 ﻿Console.Clear();
 
-Console.WriteLine("Enter coordinates XY for two points(A and B). ");
+Console.WriteLine("Enter coordinates XYZ for two points(A and B). ");
 
 Console.Write("Enter Xa = ");
 int xa = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter Ya = ");
 int ya = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Za = ");
+int za = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Enter Xb = ");
 int xb = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter Yb = ");
 int yb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Zb = ");
+int zb = Convert.ToInt32(Console.ReadLine());
 
-double d = Math.Sqrt(Math.Pow(xa-xb, 2) + Math.Pow(ya-yb, 2));
+Console.Clear();
 
-Console.WriteLine($"The distance from point A to point B is {Math.Round(d, 2)}");
+double d = Math.Sqrt(Math.Pow(xb-xa, 2) + Math.Pow(yb-ya, 2) + Math.Pow(zb - za, 2));
+
+Console.WriteLine($"The distance from point A[{xa},{ya},{za}] to point B[{xb},{yb},{zb}] equals {Math.Round(d, 2)}");
