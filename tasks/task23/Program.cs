9652b1b4ceb91f9a5cf0a9f2ba8b2e30f0aaac0d
e1win <e1win@mail.ru>
2023-05-26 19:47:22 +0300

    added task23. Math.Pow 3 + negative numbers

---
+++ tasks/task23/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter a number. ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+if (N>0)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        Console.Write($"{Math.Pow(i, 3)} ");
+    }
+}
+else
+{
+    for (int i = 1; i >= N; i--)
+    {
+        Console.Write($"{Math.Pow(i, 3)} ");
+    }
+}
+
+Console.WriteLine();
