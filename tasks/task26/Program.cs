06f23a8d53effd6752128d6cb037dbf018eae573
e1win <e1win@mail.ru>
2023-08-04 18:50:14 +0300

    added Task26. count of digits in number

---
+++ tasks/task26/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter a number. ");
+
+int x = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+Console.Clear();
+
+
+int Dig(int number)
+{
+    int i = 0;
+    
+    if (number == 0)
+    {
+        i=1;
+    }
+    else
+    {
+        while (number > 0)
+    {   number = number/10;
+        i++;
+    }
+    }
+    return i;
+}
+
+int DIG = Dig(x);
+// for (int count = 0; x > 0; count++)
+// {
+//     x = x/10;
+// }
+
+Console.Write($"The number of digits in x equals {DIG} ");
+Console.WriteLine();
