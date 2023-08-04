45ffdb68ac4b8ffb9882eed9d6d08b4c4aa26121
e1win <e1win@mail.ru>
2023-08-04 19:52:09 +0300

    added task 28. factorial fro 1 to N

---
+++ tasks/task28/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter a number. ");
+
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+
+int Fuck(int num)
+{   
+    int Fuuu = 1;
+    if (num > 0)
+    {
+        for (int i =1; i <= num; i++)
+        {
+            Fuuu = Fuuu * i;
+        }
+    }
+    else
+    {   
+        Fuuu = 0;
+    }
+    return Fuuu;
+}
+
+int FUCK = Fuck(x);
+
+Console.Write($"Fucktorial from 'x' equals {FUCK} ");
+Console.WriteLine();
