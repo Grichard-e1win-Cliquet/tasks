37cf56838223c427812d284dca96afe4f3ab9f13
e1win <e1win@mail.ru>
2023-08-09 22:29:45 +0300

    added task 25.b with function double P(int x, int y)

---
+++ tasks/task25.b/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter two numbers.");
+
+Console.WriteLine("Enter A:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter B:");
+int B = Convert.ToInt32(Console.ReadLine());
+
+double C = P(A, B);
+
+double P(int x, int y)
+{
+    double P = x;
+    if(y == 0)
+{
+    P = 1;
+    return P;
+}
+else
+{
+    if(y > 0)
+    {
+        for(int count = 1; count < y; count++)
+        {
+        P = P*x;
+        }
+        return P;
+    }
+    else
+    {
+        for(int count = 1; count < Math.Abs(y); count++)
+        {
+        P = P*x;
+        }
+        P = 1/P;
+        return P;
+    }   
+    return P;
+}
+return P;
+}
+
+Console.Write($"{A} to degree {B} equals {C}.");
+Console.WriteLine();
