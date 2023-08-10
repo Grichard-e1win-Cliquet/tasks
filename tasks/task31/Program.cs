40d5e4de0b586bf0154c43631fd1eb4ac851b996
e1win <e1win@mail.ru>
2023-08-10 17:45:43 +0300

    did task 31

--- tasks/task31/Program.cs
+++ tasks/task31/Program.cs
@@ -12,8 +12,31 @@ void FillArr(int [] array)
     }
 }
 
+void SUM (int [] array)
+{
+    int SUMPOS = 0;
+    int SUMNEG = 0;
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] >= 0)
+        {
+            SUMPOS = SUMPOS + array[i];
+        }
+        else
+        {
+            SUMNEG = SUMNEG + array[i];
+        }
+    }
+    Console.WriteLine($"The sum of positives numbers in array equals {SUMPOS}. ");
+    Console.WriteLine($"The sum of negatives numbers in array equals {SUMNEG}. ");
+}
+
 FillArr(arr);
 
 Console.WriteLine("Вывод массива: ");
-Console.Write('[' + string.Join(", ", arr) + ']');
+Console.WriteLine('[' + string.Join(", ", arr) + ']');
+
+SUM(arr);
+
 Console.WriteLine();
