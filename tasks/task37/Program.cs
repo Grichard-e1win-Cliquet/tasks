0d8ff4d75c9267152769f918a082874382ecfb5b
e1win <e1win@mail.ru>
2023-08-14 17:06:51 +0300

    added task 37. multiplied massive

---
+++ tasks/task37/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.Clear();
+Console.Write("Enter a length of massive: ");
+
+int length = int.Parse(Console.ReadLine());
+int [] array = RandomMassive(length);
+
+int [] RandomMassive (int Length)
+{
+    int [] arr = new int [Length];
+    
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-9, 10);
+    }
+    return  arr;
+}
+
+Console.WriteLine("Massive is: " + '[' + string.Join (",", array)+ ']' + ".");
+
+int[] Multiarr (int[] arr)
+{
+    if(arr.Length%2==0 && arr.Length > 2)
+    {
+        int [] Multi = new int[arr.Length/2];
+        int j = arr.Length-1;
+        for(int i=0; i<(arr.Length/2);i++)
+        {
+            Multi[i] = arr[i]*arr[j-i];
+        }
+        return Multi;
+    }
+    else if(arr.Length == 2)
+    {
+        int [] Multi = new int[1];
+        Multi[0] = arr[0]*arr[1];
+        return Multi;
+    }
+    else
+    {
+        int [] Multi = new int[((arr.Length-1)/2)+1];
+        int j = arr.Length-1;
+        for(int i=0; i<(((arr.Length-1)/2));i++)
+        {
+            Multi[i] = arr[i]*arr[j-i];
+        }
+        Multi[(arr.Length-1)/2] = arr[(arr.Length-1)/2];
+        return Multi;
+    }
+}
+
+int [] Multiplied = Multiarr(array);
+
+Console.WriteLine("New massive with multiplied pairs is: " + '[' + string.Join (",", Multiplied)+ ']' + ".");
+
