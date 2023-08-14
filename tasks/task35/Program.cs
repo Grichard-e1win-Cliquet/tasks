3761a1930e63bfd656dab13e19949552f92ee60a
e1win <e1win@mail.ru>
2023-08-14 15:35:57 +0300

    added task 35. int[]arr random massive + finding quantity of numbers in diaposon

---
+++ tasks/task35/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+Console.Write("Enter a length of massive: ");
+
+int length = int.Parse(Console.ReadLine());
+int [] array = RandomMassive(length);
+
+int [] RandomMassive (int Length)
+{
+    int [] arr = new int [Length];
+    
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-999, 1000);
+    }
+    return  arr;
+}
+
+Console.WriteLine("Massive is: " + '[' + string.Join (",", array)+ ']' + ".");
+
+Console.WriteLine("Enter diaposone where will we find quantity of elements.");
+
+int [] diaposon = new int [2];
+
+Console.Write("Enter from: ");
+diaposon[0] = int.Parse(Console.ReadLine());
+Console.Write("Enter to: ");
+diaposon[1] = int.Parse(Console.ReadLine());
+
+int count = Count(diaposon, array);
+
+int Count(int[] diapos, int [] arr)
+{
+    int howmuch = 0;
+
+    for(int i=0; i<arr.Length;i++)
+    {
+        if(arr[i] >= diapos[0] && arr[i] <= diapos[1])
+        {
+            howmuch+=1;
+        }
+    }
+    return howmuch;
+}
+
+Console.WriteLine("The qunatity of elements in diaposone" + '[' + string.Join (",", diaposon) + ']' + $" in our massive equals {count}.");
