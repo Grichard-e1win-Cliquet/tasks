b79d8c8808bdfff806ae815662b53d7806fca2f7
e1win <e1win@mail.ru>
2023-08-23 14:13:14 +0300

    added task 34 with count even numbers in [].

---
+++ tasks/task34/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+
+int length = new Random().Next(1, 101);
+int [] arr = randomArr (length);
+
+int [] randomArr (int Length)
+{
+    int [] arr = new int [Length];
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+    return arr;
+}
+
+Console.WriteLine("The massive is: " + '[' + string.Join (",", arr) + ']' + ".");
+
+int count = evennumbers (arr);
+
+int evennumbers (int [] array)
+{
+    int count = 0;
+
+    for(int i = 0; i<array.Length; i++)
+    {
+        if(array[i]%2==0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine($"The massive has {count} even numbers.");
