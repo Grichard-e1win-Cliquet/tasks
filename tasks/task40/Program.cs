304ba4585d4e0bd7041973bb54f39fb3277c1606
e1win <e1win@mail.ru>
2023-10-20 19:35:06 +0300

    added task 40 with triangle

---
+++ tasks/task40/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Enter a three length of triangle. ");
+
+Console.WriteLine("Enter A: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter B: ");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter C: ");
+int C = int.Parse(Console.ReadLine());
+
+if(A + B > C && B + C > A && C + A > B) Console.WriteLine($"Triangle with sides {A}, {B} and {C} can exist.");
+else 
+{
+    Console.WriteLine($"Triangle with sides {A}, {B} and {C} cannot exist.");
+}
