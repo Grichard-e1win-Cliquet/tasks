2da2f66d2fa4330db463648f89567ced6eaec258
e1win <e1win@mail.ru>
2023-10-21 00:05:16 +0300

    added task 42 with simple to double

---
+++ tasks/task42/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter a number: ");
+int num = int.Parse(Console.ReadLine());
+
+d(num);
+
+int d(int n)
+{
+    if(n % 2 == 0) return 0;
+    else return 1;
+}
+
+for (int i = num; i > 0; i/=2)
+{
+    Console.WriteLine($"d({i}) = {d(i)}");
+}
