698e9f4230a0da2a8849dfeb628bfb4d92f6bff7
e1win <e1win@mail.ru>
2023-10-20 18:52:48 +0300

    added task39 with my decision

---
+++ tasks/task39/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+Console.Write("Enter a length of massive: ");
+
+int length = int.Parse(Console.ReadLine());
+int [] array = RandomMassive(length);
+
+int [] RandomMassive (int Length)
+{
+    int [] arr = new int [Length];
+    
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 100);
+    }
+    return  arr;
+}
+
+Console.WriteLine("Massive is: " + '[' + string.Join (",", array)+ ']' + ".");
+
+int [] reverse = ReverseMassive(array);
+
+int [] ReverseMassive(int [] arr)
+{
+    int j = arr.Length - 1;
+
+    for (int i = 0; i < arr.Length/2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        j--;
+    }
+    return arr;
+}
+
+Console.WriteLine("The reversed massive is" + '[' + string.Join (",", reverse) + ']' + ".");
