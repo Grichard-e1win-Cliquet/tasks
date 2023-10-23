3e4f53f93e8b28418e59149ec0849c2decf3d121
e1win <e1win@mail.ru>
2023-10-23 23:07:31 +0300

    added task 44 and 45. 45 was finished

---
+++ tasks/task44/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+
+Console.Write("Enter a quantity of numbers Fibonacci: ");
+int quantity = int.Parse(Console.ReadLine());
+
+void FillArray(int length)
+{
+    int [] arr = new int [length];
+
+    if(length >= 2)
+    {
+            arr [0] = 0;
+            arr [1] = 1;
+        Console.Write($"{arr[0]}, {arr[1]}, ");
+
+    for(int i = 2; i < arr.Length; i++)
+    {
+        arr [i] = arr[i-1] + arr[i-2];
+        Console.Write($"{arr[i]}, ");
+
+    }
+    }
+    else 
+    {
+        arr [0] = 0;
+        Console.Write($"{arr[0]} ");
+    }
+}
+
+FillArray(quantity);
+Console.WriteLine();
+
+// for(i=0; i<quantity;i++)
+// {
+//     int j = i +
+//     Console.Write($"")
+// }
---
+++ tasks/task45/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+Console.WriteLine("Enter length: ");
+int length = int.Parse(Console.ReadLine());
+
+int [] arr = array(length);
+
+int [] array (int size)
+{
+    int [] array = new int [size];
+
+    for(int i=0; i<array.Length;i++)
+    {
+        array[i] = new Random().Next(10,1000);
+    }
+    return array;
+}
+Console.WriteLine();
+Console.WriteLine("The random massive is: " + '[' + string.Join(", ", arr) + ']' + ".");
+
+copy(arr);
+
+void copy(int [] arr)
+{
+    int [] newarr = new int [arr.Length];
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+       newarr [i] = arr [i];
+    }
+
+    Console.WriteLine("The copied arr is: " + '[' + string.Join(", ", newarr) + ']' + ".");
+}
