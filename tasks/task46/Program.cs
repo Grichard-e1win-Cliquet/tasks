3916e016d2be58986e0428e2e41cc0125282a6e1
e1win <e1win@mail.ru>
2023-10-28 17:27:21 +0300

    added task 46 with creating the double massive

---
+++ tasks/task46/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+
+Console.Write("Enter the quantity of lines: ");
+int lines = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the quantity of cons: ");
+int cons = int.Parse(Console.ReadLine());
+
+int [,] massive = new int [lines, cons];
+
+void FillArray(int [,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr [i, j] = new Random().Next(-10, 11);
+        }
+    }
+}
+
+void PrintArray(int [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+    Console.Write("|");
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+    Console.WriteLine("|");
+    }   
+}
+
+FillArray(massive);
+PrintArray(massive);
