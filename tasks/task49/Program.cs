336b40f9bf423133a4bf6a4438f54b7e84ee700b
e1win <e1win@mail.ru>
2023-10-28 19:50:13 +0300

    added task 49 when i doubled numbers with counts indexes

---
+++ tasks/task49/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.Clear();
+
+Console.Write("Enter quantity of lines: ");
+int lines = int.Parse(Console.ReadLine());
+
+Console.Write("Enter quantity of cons: ");
+int cons = int.Parse(Console.ReadLine());
+
+int [,] massive = new int [lines, cons];
+
+void FillArray(int [,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j<arr.GetLength(1); j++)
+        {
+            arr [i, j] = new Random().Next(-9, 10);
+        }
+    }
+}
+
+void PrintArray(int [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("| ");
+        for(int j = 0; j<array.GetLength(1); j++)
+        {            
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+FillArray(massive);
+PrintArray(massive);
+
+int [,] doubled(int [,] somemassive)
+{
+    int [,] newarray = somemassive;
+
+    for(int i = 0; i < newarray.GetLength(0); i++)
+    {
+        for(int j = 0; j<newarray.GetLength(1); j++)
+        {
+            if( i%2 == 0 && j%2==0)
+            {
+                newarray[i, j] = newarray[i, j] * newarray[i, j];
+            }
+        }
+    }
+
+return newarray;
+}
+
+int [,] newMassive = doubled(massive);
+
+
+Console.WriteLine();
+PrintArray(newMassive);
