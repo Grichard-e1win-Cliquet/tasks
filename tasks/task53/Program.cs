75f9e9fa4bf26dd1b8b1c5ece700e4078ec2f16a
e1win <e1win@mail.ru>
2023-11-11 17:44:35 +0300

    added task 52 swap first with last lines

---
+++ tasks/task53/Program.cs
@@ -0,0 +1,61 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter quantity of lines and cons.");
+
+Console.Write("Quantity of lines: ");
+int lines = int.Parse(Console.ReadLine());
+
+Console.Write("Quantity of cons: ");
+int cons = int.Parse(Console.ReadLine());
+
+int [,] massive = enterArr(lines, cons);
+
+int [,] enterArr(int line, int con)
+{
+    int [,] array = new int [line, con];
+
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array [i, j] = new Random().Next(-9, 10);
+        }
+    }
+    return array;
+}
+
+
+void PrintArray(int [,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write("| ");
+
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"({arr [i, j]}) ");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+PrintArray(massive);
+Console.WriteLine();
+
+void Swap(int [,] mass)
+{   
+    for(int i = 0; i < 1; i++)
+    {
+        int temp = 0;
+        for(int j = 0; j < mass.GetLength(1); j++)
+        {
+            temp = mass[i, j];
+            mass [i, j] = mass [mass.GetLength(0)-1, j];
+            mass [mass.GetLength(0)-1, j] = temp;
+        }
+        // Console.Write($"({temp/mass.GetLength(0):f2}) ");
+    }
+    PrintArray(mass);
+}
+
+Swap(massive);
