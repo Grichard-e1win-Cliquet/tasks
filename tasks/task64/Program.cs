0fbaa51622197049db8094b1fdd7264a68ea67a3
e1win <e1win@mail.ru>
2023-12-02 19:44:24 +0300

    added task 64 naturals nambers from N to 1

---
+++ tasks/task64/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Enter a number: ");
+
+int N = int.Parse(Console.ReadLine());
+naturals(N);
+void naturals(int n)
+{
+    if(n == 0) return;
+    Console.Write($"{n} ");
+    naturals(n-1);
+}
+Console.WriteLine();
